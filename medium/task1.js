a73b3613dd31221cc1735035b3e6c6f56865e0e5
Tausif Alam <devops.tausifalam@outlook.com>
2025-08-27 22:46:59 +0530

    task1-medium-lvl-pre-done

---
+++ medium/task1.js
@@ -0,0 +1,62 @@
+/* 
+
+Library Book Management (In-Memory)
+Create a program where a user can:
+•	Add books (title, author)
+•	Search book by title
+•	Show all books
+You don’t need a database, just keep data in memory (using List or Map).
+Structuring data, Lists, OOP
+
+Author: Tausif Alam
+**** Use Node.js to run this javascript :)
+
+*/
+
+class Books {
+    constructor (title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+class Library {
+    constructor () {
+        this.books = [];
+    }
+
+    // add books to library
+    addBooks(title,author) {
+        const book = new Books(title, author);
+        this.books.push(book);
+
+        // prompt 
+        console.log(`😍 reader's now have a new book by, ${author}`);
+    }
+
+    // search for books by title
+    searchBook(title) {
+        const result = this.books.filter(book => {
+            book.title.toLowerCase().includes(title.toLowerCase());
+        });
+
+        if (result.length > 0) {
+            result.forEach(item => {
+                console.log(`Found books relating to your query: ${item.title} by ${item.author}`)
+            })
+        } else {
+            console.log("No Books Found 🙅🚫");
+        }
+    }
+
+    // showAllBooks
+    showAllBooks(title, author) {
+        if (this.books.length > 0) {
+            this.books.forEach((b, index) => {
+            console.log("Available Books 👇");
+            console.log(`${index+1}. ${b.title} -by ${b.author}`);
+            })
+        } else {
+            console.log("Donate some books! No books available 🚫")
+        }
+    }
+}
