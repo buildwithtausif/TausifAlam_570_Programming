9a1ee63ee3dc36485fb23ff73b0adbcc426cd593
Tausif Alam <devops.tausifalam@outlook.com>
2025-08-27 22:53:27 +0530

    easy-lvl init

---
+++ easy/task1_wordFreq.js
@@ -0,0 +1,12 @@
+/*
+Word Frequency Counter
+Take a paragraph of text as input and print the top 3 most frequent words.
+HashMap, Sorting, Strings
+
+Author: Tausif Alam
+**** use nodejs to run the code
+*/
+
+function wordCount() {
+    
+}
